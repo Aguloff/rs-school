1d5b3b28b7a2e20b325eaac669c7639813ad4e01
Aguloff <agulovu@gmail.com>
2023-08-01 18:41:44 +0300

    add burger-menu

--- library/index.js
+++ library/index.js
@@ -1,1 +1,15 @@
 console.log('Вроде всё соответствует заданию, 100/100');
+
+const burgerBtn = document.querySelector('.burger');
+const burgerMenu = document.querySelector('.nav');
+const menuLinks = document.querySelectorAll('.nav-link');
+
+function burger() {
+    burgerBtn.classList.toggle('burger--active');
+    burgerMenu.classList.toggle('nav--active');
+    document.body.classList.toggle('stop-scroll');
+}
+
+burgerBtn.addEventListener('click', () => burger());
+
+menuLinks.forEach(link => link.addEventListener('click', () => burger()));
