5ddef0e54b6129fdbc729154a7734e365cb1f9f0
Aguloff <agulovu@gmail.com>
2023-08-04 21:59:25 +0300

    feat: add close burger-menu on click on body

--- library/index.js
+++ library/index.js
@@ -1,15 +1,28 @@
-console.log('Вроде всё соответствует заданию, 100/100');
-
 const burgerBtn = document.querySelector('.burger');
 const burgerMenu = document.querySelector('.nav');
 const menuLinks = document.querySelectorAll('.nav-link');
+const profileBtn = document.querySelector('.profile-btn');
+const modal = document.querySelector('.modal');
 
 function burger() {
     burgerBtn.classList.toggle('burger--active');
+    profileBtn.classList.toggle('profile-btn--transform');
     burgerMenu.classList.toggle('nav--active');
     document.body.classList.toggle('stop-scroll');
+    modal.classList.toggle('modal--active');
 }
 
 burgerBtn.addEventListener('click', () => burger());
 
+burgerMenu.addEventListener('click', (e) => {
+    e._click = true;
+});
+
+modal.addEventListener('click', (e) => {
+    if (e._click) return;
+    burger();
+});
+
 menuLinks.forEach(link => link.addEventListener('click', () => burger()));
+
+console.log('1. Вёрстка соответствует макету. Ширина экрана 768px +26\n2. Ни на одном из разрешений до 640px включительно не появляется горизонтальная полоса прокрутки. Весь контент страницы при этом сохраняется: не обрезается и не удаляется +12\n3. На ширине экрана 768рх реализовано адаптивное меню +12\n\nРезультат: 50/50');
